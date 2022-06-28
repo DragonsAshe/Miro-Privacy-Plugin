3471f0f228189855c3536a88946c09563e065649
Solid <solidgitmail@googlemail.com>
2022-06-28 09:37:48 +0200

    Read me update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,39 @@
 async function init() {
-  miro.board.ui.on('icon:click', async () => {
-    await miro.board.ui.openPanel({url: 'app.html'});
+  miro.board.ui.on("icon:click", async () => {
+
+    // Get selected items
+    let selectedItems = await miro.board.getSelection()
+
+    // Filter sticky notes from selected items
+    let stickyNotes = selectedItems.filter((item) => item.type === 'sticky_note')
+
+    // Delete selected stickers
+    for (const stickyNote of stickyNotes) {
+      await miro.board.remove(stickyNote) 
+    }
+
+
+    // Create sticky note that hides the text
+    for (const stickyNote of stickyNotes) {
+      if(parseInt(stickyNote.width) == 6){
+        await miro.board.createStickyNote({
+          content: stickyNote.content,
+          x: stickyNote.x,
+          y: stickyNote.y,
+          width: 199,
+          height: stickyNote.height,
+        }) 
+      } else {
+        await miro.board.createStickyNote({
+          content: stickyNote.content,
+          x: stickyNote.x,
+          y: stickyNote.y,
+          width: 6,
+          height: stickyNote.height,
+        }) 
+      }
+    }
+
   });
 }
 
